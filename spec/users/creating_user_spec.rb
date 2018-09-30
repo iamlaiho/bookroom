cbdb71753c87a95d9021751a327d74e84044e70b
laiho <laiho.lim@gmail.com>
2018-09-30 20:42:47 +0800

    Remove Rspec

--- spec/users/creating_user_spec.rb
+++ spec/users/creating_user_spec.rb
@@ -1,10 +1,3 @@
 require 'rails_helper.rb'
 
-feature 'Creating user' do
-	scenario 'can create a user' do
-		visit '/users/new'
-		fill_in 'user_name', with: 'testuser1'
-		click_button 'Save User'
-		expect(page).to have_content('testuser1')
-	end
 end
