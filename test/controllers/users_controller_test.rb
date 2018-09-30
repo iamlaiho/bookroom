189cf6264413247864fb47d5f3e05145273f5d9f
laiho <laiho.lim@gmail.com>
2018-09-30 13:56:51 +0800

    Added test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+	test "should get index" do
+		get users_url
+		assert_response :success
+	end
+end
+
+require 'test_helper'
+
+class BookingsControllerTest < ActionDispatch::IntegrationTest
+	test "should get index" do
+		puts "booking"
+		assert_redirected_to bookings_url
+	end
 end
